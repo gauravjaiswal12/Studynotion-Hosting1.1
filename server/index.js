05eada549f8b30c24b03512e5cca22969eb08712
GauravJaiswal <gauravjaiswal.iiita@gmail.com>
2025-04-16 12:51:40 +0530

    Cors is updated to run along with vercel

--- server/index.js
+++ server/index.js
@@ -23,12 +23,23 @@ database.connect();
 //middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors(
-    {
-        origin: "http://localhost:3000",
-        credentials: true,
-    }
-));
+const allowedOrigins = [
+    "http://localhost:3000",
+    "https://studynotion-hosting1-1.vercel.app"
+];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        // Allow requests with no origin (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) !== -1) {
+            return callback(null, true);
+        } else {
+            return callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+}));
 app.use(
     fileUpload({
         useTempFiles:true,
